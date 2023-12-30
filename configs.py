3fb60a84b5ebb0bc7153b34d32b667cc61e58443
seungyeon <jq3210@naver.com>
2023-12-30 17:25:02 +0900

    change config.py

--- configs.py
+++ configs.py
@@ -4,18 +4,19 @@ config_preprocessing = {
     # "annotation_path": r"H:\Affwild2\annotations\VA_Set",
     # "image_path": r"H:\Affwild2\cropped_aligned",
     # "output_path": r"H:\Affwild2_processed",
-    "raw_video_path": r"../data/Affwild2/train_video/batch1",
+    "raw_video_path": r"../data/Affwild2/raw_video",
     "test_video_path": r"../data/Affwild2/test_video/new_vids",
     "annotation_path": r"../data/Affwild2/annotations/VA_Set",
-    "image_path": r"../data/Affwild2/cropped_aligned_images1/cropped_aligned",
+    "image_path": r"../data/Affwild2/resized_cropped_aligned_images",
+    # "image_path": r"../data/Affwild2/resized_cropped_aligned_images_test",
     "output_path": r"output",
 
     # "aural_feature_list": ["mfcc", "egemaps", "vggish"],
     # "opensmile_exe_path": r"D:\opensmile-3.0-win-x64\bin\SMILExtract.exe",
     # "opensmile_config_path": r"D:\opensmile-3.0-win-x64\config",
     "aural_feature_list": ["mfcc", "egemaps", "vggish"],
-    "opensmile_exe_path": r"ABAW2/opensmile-3.0-linux-x64/bin/SMILExtract",
-    "opensmile_config_path": r"ABAW2/opensmile-3.0-linux-x64/config",
+    "opensmile_exe_path": r"opensmile-3.0-linux-x64/bin/SMILExtract",
+    "opensmile_config_path": r"opensmile-3.0-linux-x64/config",
 
     # "openface_config": {
     #         "openface_directory": "D:\\OpenFace-master\\x64\\Release\\FeatureExtraction",
@@ -30,7 +31,7 @@ config_preprocessing = {
     #         "output_directory_flag": " -out_dir "
     #     }
     "openface_config": {
-            "openface_directory": "ABAW2/OpenFace-OpenFace_2.2.0/exe/FeatureExtraction",
+            "openface_directory": "OpenFace-OpenFace_2.2.0/exe/FeatureExtraction",
             "input_flag": " -f  ",
             "output_features": " -2Dfp ",
             "output_action_unit": " -aus ",
@@ -48,15 +49,15 @@ config_processing = {
 }
 
 
-# I extracted vggish feature using remote server.
+# # I extracted vggish feature using remote server.
 # config_preprocessing = {
 #     "raw_video_path": r"/home/zhangsu/affwild/raw_video",
 #     "annotation_path": r"/home/zhangsu/affwild/annotations/VA_Set",
 #     "image_path": r"H:\Affwild2\cropped_aligned",
 #     "output_path": r"/home/zhangsu/affwild_processed",
-#
+
 #     "aural_feature_list": ["mfcc", "vggish"],
 #     "opensmile_exe_path": r"D:\opensmile-3.0-win-x64\bin\SMILExtract.exe",
 #     "opensmile_config_path": r"D:\opensmile-3.0-win-x64\config",
-#
+
 # }
