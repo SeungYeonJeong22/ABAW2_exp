d35658f59847a3a9905219a9da7df07dfe5a879f
seungyeonjeong22 <jq3210@gmail.com>
2024-04-19 15:05:30 +0900

    M: Before Modify Split Partiton

--- base/dataset.py
+++ base/dataset.py
@@ -8,7 +8,7 @@ from torchvision.transforms import transforms
 import numpy as np
 import random
 from operator import itemgetter
-
+from sklearn.model_selection import KFold
 
 class ABAW2_VA_Arranger(object):
     def __init__(self, dataset_path, window_length=300, hop_length=300, debug=0):
@@ -21,12 +21,12 @@ class ABAW2_VA_Arranger(object):
 
     @staticmethod
     def generate_partition_dict_for_cross_validation(partition_dict, fold):
-        new_partition_dict = {'Train_Set': {}, 'Validation_Set': {}, 'Target_Set': {}}
+        new_partition_dict = {'Train_Set': {}, 'Validation_Set': {}, 'Test_Set': {}}
         partition_pool = {**partition_dict['Train_Set'], **partition_dict['Validation_Set']}
 
         trials_of_train_set = list(partition_dict['Train_Set'].keys())
         trials_of_original_validate_set = list(partition_dict['Validation_Set'].keys())
-        trials_of_putative_test_set = list(partition_dict['Target_Set'].keys())
+        trials_of_putative_test_set = list(partition_dict['Test_Set'].keys())
 
         fold_0_trials = trials_of_train_set[slice(0, 70)]
         fold_1_trials = trials_of_train_set[slice(70, 140)]
@@ -54,49 +54,159 @@ class ABAW2_VA_Arranger(object):
                     partition = "Validation_Set"
                 new_partition_dict[partition].update({trial: partition_pool[trial]})
 
-        new_partition_dict['Target_Set'] = partition_dict['Target_Set']
+        new_partition_dict['Test_Set'] = partition_dict['Test_Set']
         return new_partition_dict
+    
+    @staticmethod
+    def split_train_test_trial(partition_dict, mode='Test', seed=0):
+        random.seed(seed)
+        
+        # Train_Test를 나눌 때
+        if mode == 'Test':
+            new_partition_dict = {'Train_Set': {}, 'Test_Set': {}}
+            
+            # 전체 데이터로 모아서
+            partition_dict['Train_Set'].update(partition_dict['Validation_Set'])
+            partition_dict = partition_dict['Train_Set']
+            
+            # 20%만 테스트로 사용
+            test = random.sample(partition_dict.keys(), int(len(partition_dict.keys()) * 0.2))
+            train = [t for t in partition_dict.keys() if not t in test]
+            
+            for trial_train in train:
+                new_partition_dict["Train_Set"].update({trial_train:partition_dict[trial_train]})
+                
+            for trial_test in test:
+                new_partition_dict["Test_Set"].update({trial_test:partition_dict[trial_test]})
+                
+            return new_partition_dict
+        
+        # Train_Valid 나눌 때
+        elif mode == 'Valid':
+            new_partition_dict = {'Train_Set': {}, 'Validation_Set': {}}
+            
+            # 20%만 테스트로 사용
+            valid = random.sample(partition_dict.keys(), int(len(partition_dict.keys()) * 0.2))
+            train = [t for t in partition_dict.keys() if not t in valid]
+            
+            for trial_train in train:
+                new_partition_dict["Train_Set"].update({trial_train:partition_dict[trial_train]})
+                
+            for trial_valid in valid:
+                new_partition_dict["Validation_Set"].update({trial_valid:partition_dict[trial_valid]})
+                
+            partition_dict = new_partition_dict
+                
+            return partition_dict            
+            
+    
+    def custom_sampled_list(self, sampled_list, partition, trial, length):
+        start = 0
+        end = start + self.window_length
 
-    def resample_according_to_window_and_hop_length(self, fold):
-        partition_dict = self.dataset_info['partition']
-
-        partition_dict = self.generate_partition_dict_for_cross_validation(partition_dict, fold)
-        sampled_list = {'Train_Set': [], 'Validation_Set': [], 'Target_Set': []}
-
-        for partition, trials in partition_dict.items():
-            trial_count = 0
-            for trial, length in trials.items():
-
-                start = 0
+        if end < length:
+            # Windows before the last one
+            while end < length:
+                indices = np.arange(start, end)
+                path = os.path.join(self.dataset_path, trial)
+                sampled_list[partition].append([path, trial, indices, length])
+                start = start + self.hop_length
                 end = start + self.window_length
 
-                if end < length:
-                    # Windows before the last one
-                    while end < length:
-                        indices = np.arange(start, end)
-                        path = os.path.join(self.dataset_path, trial)
-                        sampled_list[partition].append([path, trial, indices, length])
-                        start = start + self.hop_length
-                        end = start + self.window_length
-
-                    # The last window ends with the trial length, which ensures that no data are wasted.
-                    start = length - self.window_length
-                    end = length
-                    indices = np.arange(start, end)
-                    path = os.path.join(self.dataset_path, trial)
-                    sampled_list[partition].append([path, trial, indices, length])
-                else:
-                    end = length
-                    indices = np.arange(start, end)
-                    path = os.path.join(self.dataset_path, trial)
-                    sampled_list[partition].append([path, trial, indices, length])
-
-                trial_count += 1
-
-                if self.debug and trial_count >= self.debug:
-                    break
-
+            # The last window ends with the trial length, which ensures that no data are wasted.
+            start = length - self.window_length
+            end = length
+            indices = np.arange(start, end)
+            path = os.path.join(self.dataset_path, trial)
+            sampled_list[partition].append([path, trial, indices, length])
+        else:
+            end = length
+            indices = np.arange(start, end)
+            path = os.path.join(self.dataset_path, trial)
+            sampled_list[partition].append([path, trial, indices, length])
+            
         return sampled_list
+    
+
+    def resample_according_to_window_and_hop_length(self, seed=0):
+        partition_dict = self.dataset_info['partition']
+
+        # partition_dict = self.generate_partition_dict_for_cross_validation(partition_dict, fold)
+        
+        # train / test
+        partition_dict = self.split_train_test_trial(partition_dict, mode='Test', seed=seed)
+        partition_dict_test = partition_dict['Test_Set']
+        
+        # train / valid 
+        partition_dict = self.split_train_test_trial(partition_dict['Train_Set'], mode='Valid', seed=seed)
+        partition_dict['Test_Set'] = partition_dict_test
+
+        sampled_list_dict = {'Train_Set': [], 'Validation_Set': [], 'Test_Set':[]}
+        for train_trials in partition_dict['Train_Set']:
+            trial_count = 0
+            partition = "Train_Set"
+            length = partition_dict['Train_Set'][train_trials]
+            self.custom_sampled_list(sampled_list_dict, partition, train_trials, length)
+            trial_count += 1
+            if self.debug and trial_count >= self.debug:
+                break
+            
+        for valid_trials in partition_dict['Validation_Set']:
+            trial_count = 0
+            partition = "Validation_Set"
+            length = partition_dict['Validation_Set'][valid_trials]
+            self.custom_sampled_list(sampled_list_dict, partition, valid_trials, length)
+            trial_count += 1
+            if self.debug and trial_count >= self.debug:
+                break
+            
+        for test_trials in partition_dict['Test_Set']:
+            trial_count = 0
+            partition = "Test_Set"
+            length = partition_dict['Test_Set'][test_trials]
+            self.custom_sampled_list(sampled_list_dict, partition, test_trials, length)
+            trial_count += 1
+            if self.debug and trial_count >= self.debug:
+                break                        
+            
+        return sampled_list_dict
+        
+        # sampled_list = {'Train_Set': [], 'Validation_Set': [], 'Test_Set': []}
+
+        # for partition, trials in partition_dict.items():
+        #     trial_count = 0
+        #     for trial, length in trials.items():
+
+        #         start = 0
+        #         end = start + self.window_length
+
+        #         if end < length:
+        #             # Windows before the last one
+        #             while end < length:
+        #                 indices = np.arange(start, end)
+        #                 path = os.path.join(self.dataset_path, trial)
+        #                 sampled_list[partition].append([path, trial, indices, length])
+        #                 start = start + self.hop_length
+        #                 end = start + self.window_length
+
+        #             # The last window ends with the trial length, which ensures that no data are wasted.
+        #             start = length - self.window_length
+        #             end = length
+        #             indices = np.arange(start, end)
+        #             path = os.path.join(self.dataset_path, trial)
+        #             sampled_list[partition].append([path, trial, indices, length])
+        #         else:
+        #             end = length
+        #             indices = np.arange(start, end)
+        #             path = os.path.join(self.dataset_path, trial)
+        #             sampled_list[partition].append([path, trial, indices, length])
+
+        #         trial_count += 1
+
+        #         if self.debug and trial_count >= self.debug:
+        #             break
+
+        # return sampled_list
 
 
 class ABAW2_VA_Dataset(Dataset):
@@ -119,7 +229,7 @@ class ABAW2_VA_Dataset(Dataset):
         elif self.mode == "validate":
             partition = "Validation_Set"
         elif self.mode == "test":
-            partition = "Target_Set"
+            partition = "Test_Set"
         else:
             raise ValueError("Unknown partition!")
 
@@ -150,19 +260,35 @@ class ABAW2_VA_Dataset(Dataset):
                 ])
 
         if "mfcc" in self.modality:
-            self.mfcc_transforms = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize(mean=self.mean_std_info["mfcc"][self.fold][self.partition]['mean'],
-                                     std=self.mean_std_info["mfcc"][self.fold][self.partition]['std']),
-            ])
+            try:
+                self.mfcc_transforms = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.mean_std_info["mfcc"][self.fold]['Train_Set']['mean'],
+                                        std=self.mean_std_info["mfcc"][self.fold]['Train_Set']['std']),
+                ])
+            except:
+                self.mfcc_transforms = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.mean_std_info["mfcc"][self.fold]['Validation_Set']['mean'],
+                                        std=self.mean_std_info["mfcc"][self.fold]['Validation_Set']['std']),
+                ])
+                
 
         if "vggish" in self.modality:
-            self.vggish_transforms = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize(mean=self.mean_std_info["vggish"][self.fold][self.partition]['mean'],
-                                     std=self.mean_std_info["vggish"][self.fold][self.partition]['std']),
-            ])
-
+            try:
+                self.vggish_transforms = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.mean_std_info["vggish"][self.fold]['Train_Set']['mean'],
+                                        std=self.mean_std_info["vggish"][self.fold]['Train_Set']['std']),
+                ])
+            except:
+                self.vggish_transforms = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=self.mean_std_info["vggish"][self.fold]['Validation_Set']['mean'],
+                                        std=self.mean_std_info["vggish"][self.fold]['Validation_Set']['std']),
+                ])                
+                
+        torch.cuda.empty_cache()
 
 
     @staticmethod
@@ -190,24 +316,6 @@ class ABAW2_VA_Dataset(Dataset):
             frames = self.image_transforms(frames)
             features.update({'frame': frames})
 
-        if "fau" in self.modality:
-
-            if length < self.window_length:
-                au = np.zeros((self.window_length, 17), dtype=np.float32)
-                au[indices] = self.load_data(path, indices, "au.npy")
-            else:
-                au = self.load_data(path, indices, "au.npy")
-            features.update({'au': au})
-
-        if "flm" in self.modality:
-            if length < self.window_length:
-                landmark = np.zeros((self.window_length, 68, 2), dtype=np.float32)
-                landmark[indices] = self.load_data(path, indices, "landmark.npy")
-            else:
-                landmark = self.load_data(path, indices, "landmark.npy")
-            landmark = np.array(landmark.reshape(len(landmark), -1), dtype=np.float32)
-            features.update({'landmark': landmark})
-
         if "mfcc" in self.modality:
             if length < self.window_length:
                 mfcc = np.zeros((self.window_length, 39), dtype=np.float32)
@@ -216,14 +324,6 @@ class ABAW2_VA_Dataset(Dataset):
                 mfcc = self.load_data(path, indices, "mfcc.npy").astype(np.float32)
             mfcc = self.mfcc_transforms(mfcc)
             features.update({'mfcc': mfcc})
-
-        if "egemaps" in self.modality:
-            if length < self.window_length:
-                egemaps = np.zeros((self.window_length, 23), dtype=np.float32)
-                egemaps[indices] = self.load_data(path, indices, "egemaps.npy")
-            else:
-                egemaps = self.load_data(path, indices, "egemaps.npy")
-            features.update({'egemaps': egemaps})
 
         if "vggish" in self.modality:
             if length < self.window_length:
